b951f2bb35452a98456f0ec1fff2a2ed55c93f5f
Anton <AIMakarov@alfabank.ru>
2026-07-08 12:16:31 +0300

    WH4 part 1:
    В класс MainReflection добавил вызов метода toString с помощью рефлексии.

--- src/MainReflection.java
+++ src/MainReflection.java
@@ -2,10 +2,12 @@
 import model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -13,6 +15,8 @@ public class MainReflection {
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
         // TODO : invoke r.toString via reflection
+        Method method = r.getClass().getDeclaredMethod("toString");
         System.out.println(r);
+        System.out.println(method.invoke(r));
     }
 }
